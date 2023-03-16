2556122e21ff3d8ee94b4ec37dbc4850ca06a435
Johannes Merkt <mailjohannesmerkt@gmail.com>
2023-03-16 20:19:04 +0100

    Try mongo lib

--- main.ts
+++ main.ts
@@ -1,25 +1,8 @@
 import {
-  Database,
-  DataTypes,
-  Model,
-  SQLite3Connector,
-} from "https://deno.land/x/denodb@v1.4.0/mod.ts";
+  Bson,
+  MongoClient,
+} from "https://deno.land/x/mongo@v0.31.1/mod.ts";
 
-export class User extends Model {
-  static table = "users";
-  static timestamps = true;
-  static fields = {
-    id: { primaryKey: true, autoIncrement: true },
-    email: { type: DataTypes.STRING, unique: true },
-  };
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const connector = new SQLite3Connector({
-    filepath: "./database.sqlite",
-  });
-  const db = new Database(connector);
-  db.link([User]);
-  console.log("Success!");
+const client = new MongoClient();
 }
